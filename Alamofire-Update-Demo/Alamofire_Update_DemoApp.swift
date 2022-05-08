3284cc234397b6c74155d1ff18bc33f872e8650a
SNQ-2001 <taishin05109@gmail.com>
2022-05-09 02:40:33 +0900

    Initial Commit

---
+++ Alamofire-Update-Demo/Alamofire_Update_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Alamofire_Update_DemoApp.swift
+//  Alamofire-Update-Demo
+//
+//  Created by 宮本大新 on 2022/05/09.
+//
+
+import SwiftUI
+
+@main
+struct Alamofire_Update_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
